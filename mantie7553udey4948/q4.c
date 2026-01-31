627d53609131a70c0d2d934d8f2320a24039daa7
Jaden M <jrmantie@hotmail.com>
2026-01-30 23:51:04 -0600

    Comments for q4

--- mantie7553udey4948/q4.c
+++ mantie7553udey4948/q4.c
@@ -10,6 +10,9 @@
  * 4. A search function findRecord that returns the address of a record based on the Food Serial Number.
  * Return NULL if the Food Serial Number is not found in any record.
  *
+ * Food Category Code (FCC) – a letter indicating the food category (stored in least significant byte)
+ * Food Handling Code (FHC) – a value in the range 0 to 255
+ * Food Serial Number (FSN) – a value in the range 0 to 65535 (stored in most significant bytes)
 */
 
 unsigned int* findRecord(unsigned short FSN, unsigned int* iFoodArrayPtr, int iSize)
@@ -23,10 +26,12 @@ unsigned int* findRecord(unsigned short FSN, unsigned int* iFoodArrayPtr, int iS
         // as it is a short pointer this can be dereferenced to also get the value from the 4th byte
         unsigned short* fsnPtr = (unsigned short*)&foodItem[2];
 
+        // if the value passed in and the one we are checking match then return the address of the item in the array
         if (*fsnPtr == FSN)
         {
             return &iFoodArrayPtr[i];
         }
     }
+    // nothing was found so return NULL
     return NULL;
 }
