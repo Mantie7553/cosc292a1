b138be4be8dd007d0c62c15484f7a5a9e0b05cc6
Jaden M <jrmantie@hotmail.com>
2026-01-30 23:48:31 -0600

    Completed q2. Can now search for a record in an array with a given FSN

--- mantie7553udey4948/q4.c
+++ mantie7553udey4948/q4.c
@@ -14,5 +14,19 @@
 
 unsigned int* findRecord(unsigned short FSN, unsigned int* iFoodArrayPtr, int iSize)
 {
+    for (int i = 0; i < iSize; i++)
+    {
+        // char pointer to the current item in the array, used to break down information into separate bytes
+        char* foodItem = (char*)&iFoodArrayPtr[i];
+
+        // gets pointer to the 3rd byte
+        // as it is a short pointer this can be dereferenced to also get the value from the 4th byte
+        unsigned short* fsnPtr = (unsigned short*)&foodItem[2];
+
+        if (*fsnPtr == FSN)
+        {
+            return &iFoodArrayPtr[i];
+        }
+    }
     return NULL;
 }
